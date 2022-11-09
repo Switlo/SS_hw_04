a1f9fafb79425744912ae6ad547743041f2940b4
Svitlana R <sv73@ukr.net>
2022-11-09 21:05:08 +0200

    Fixed mistakes in 1-4 tasks; Tried to find a solution for 5 task

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const userTicket = prompt(`Введіть 6 цифр вашого квитка`)
 
 const ticketNumber = userTicket.split(``);
 
-if (ticketNumber[0] + ticketNumber[1] + ticketNumber[2] === ticketNumber[3] + ticketNumber[4] + ticketNumber[5]) {
+let firstPart = 0;
+let secondPart = 0;
+
+for (let i = 0, j = 3; i < 3; i++, j++) { 
+    firstPart += Number(ticketNumber[i]);
+    secondPart += +ticketNumber[j];   // представлені 2 варіанти переведення рядка у число
+}
+
+if (firstPart === secondPart) {
     document.querySelector("#happyTicket").innerHTML = `Вітаємо! Ваш квиток щасливий :)`
 } else { 
     document.querySelector("#happyTicket").innerHTML = `Упс... Ваш квиток нещасливий :( Купіть інший квиток!`
@@ -17,21 +25,23 @@ if (ticketNumber[0] + ticketNumber[1] + ticketNumber[2] === ticketNumber[3] + ti
 const yourString = prompt(`Введіть вислів розміром до 10 слів`);
 const stringArray = yourString.split(" ");
 
-// // console.log(stringArray[2]);
-// // console.log(stringArray[2].length);
+let lettersNumber = stringArray[0].length;
+const shortestWords = [stringArray[0]];
 
-let shortestWord = stringArray[0];
-let lettersNumber = 0;
 
 for (i = 1; i < stringArray.length; i++) { 
+    const curentLength = stringArray[i].length;
 
-    if (stringArray[i] <= shortestWord) {
-        shortestWord = stringArray[i];
-        lettersNumber = stringArray[i].length;
+    if (curentLength <= lettersNumber) {
+        if (curentLength < lettersNumber) shortestWords.length = 0; // одна дія після if виконується без {}
+        lettersNumber = curentLength;
+        if (!shortestWords.includes(stringArray[i])) { 
+            shortestWords.push(stringArray[i]);
+        }
     } 
 }
 document.querySelector("#yourString").innerHTML = `Ви ввели такий рядок: ${yourString}.`;
-document.querySelector("#shortestWord").innerHTML = `Кількість літер у найкоротшому слові: ${lettersNumber}.`
+document.querySelector("#shortestWord").innerHTML = `Кількість літер у найкоротшому слові: ${lettersNumber}. Ось ці слова: ${shortestWords}.`
 
 // ДНК
 
@@ -74,4 +84,26 @@ if (yourPlace % 2 !== 0 && yourPlace <= 36) {
     compartmentNumber = Math.floor((54 - yourPlace) / 2 + 1);
     document.querySelector("#yourPlace").innerHTML = `У вас верхнє бокове місце у купе №${compartmentNumber}.`
 }
+
+// Калькулятор
+
+// const string = prompt(`Введіть послідовно через пробіл два числа та дію, яку з ними потрібно виконати (/, *, + чи -)`);
+
+// let array = string.split(" ");
+// let lastElement = array.pop();
+// let secondElement = array.pop();
+
+// let arrayNew = array.push(lastElement, secondElement);
+
+// let stringNew = arrayNew.join(" ");
+
+// let result2 = eval(stringNew);
+
+// document.querySelector("#answer").innerHTML = `Ви ввели: ${string}. Результат: ${result2}.`
+
+// let newArray = string.split("");
+// let newArray2 = newArray.splice(1, 2, newArray[2], newArray[1]);
+
+// let array1 = array.replace(array[2], array[1]);
+// let array2 = array1.replace(array1[1], array[2]);
 
